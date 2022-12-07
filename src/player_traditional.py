683ecfa190b28e90006f0785366e71da87a02dcb
YuHaoHan <88138048+YuHaoHan@users.noreply.github.com>
2022-12-06 20:23:39 -0800

    Update player_traditional.py

--- src/player_traditional.py
+++ src/player_traditional.py
@@ -1,23 +1,13 @@
+# import library
 import pandas as pd
-import numpy as np
-import sklearn
-import seaborn as sns
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import accuracy_score, log_loss
-from sklearn.neural_network import MLPClassifier
-from sklearn.svm import LinearSVC
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
-
-
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
-#defining dataset class
+# defining dataset class
 from torch.utils.data import Dataset, DataLoader
+from pytorchtools import EarlyStopping
 
-#defining the network
+# defining the network
 from torch import nn
 from torch.nn import functional as F
 
@@ -45,11 +35,6 @@ class Net(nn.Module):
             # x = torch.relu(l(x))
         else:
             x = torch.sigmoid(l(x))
-       
-    # x = torch.relu(self.fc1(x))
-    # x = torch.relu(self.fc2(x))
-    # x = torch.relu(self.fc3(x))
-    # x = torch.sigmoid(self.fc3(x))
 
     return x
 
@@ -69,40 +54,18 @@ class dataset(Dataset):
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.max_rows', 500)
 
-
+# read the traditional data
 df = pd.read_csv('../data/NBA_Traditional_Stats_Dataset.csv')
 
+# check if there's missing data
 total = df.isnull().sum().sort_values(ascending=False)
 percent = (df.isnull().sum()/df.isnull().count()).sort_values(ascending=False)
 missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
 print("Missing Values:")
 print(missing_data.head())
 
-# player_cols=['W/L','H Win Avg', 'V Win Avg', 'H Win Avg Last 8', 'V Win Avg Last 8']
-# for c in df.columns:
-#     temp=c
-#     c=c.split(' ')
-#     if len(c)>1 and c[1]=='Player':
-#         player_cols.append(temp)
-
-
-# df= df[player_cols]
-
-
-# Correlation Matrix
-# import matplotlib.pyplot as plt
-# cmap = sns.diverging_palette(220, 10, as_cmap=True)
-# corrmat = df.corr()
-# f, ax = plt.subplots(figsize=(16, 12))
-# sns.heatmap(corrmat, cmap=cmap, vmax=.8, square=True);
-
-# print(abs(corrmat['W/L']).sort_values(ascending=False))
-
-
-
-
+# drop the data that are not our inputs
 X = df.drop(columns=['W/L', 'Match Up', 'Game Date', 'V P8 +/-'])
-print(X.shape[1])
 
 y = df['W/L']
 
@@ -155,83 +118,68 @@ model = Net(input_shape=X_train.shape[1],layers = [16])
 optimizer = torch.optim.Adam(model.parameters(),lr=learning_rate)
 loss_fn = nn.BCELoss()
 
-#forward loop
-losses_all = []
-accur_all = []
 
-for i in range(epochs):
-    model.train()
-    accur = []
-    losses = []
-    for j,(x_tr,y_tr) in enumerate(trainloader):
-        #calculate output
-        output = model(x_tr)
-        # output = model(x_train)
+def train_model(model, patience, n_epoch):
+  
+  early_stopping = EarlyStopping(patience=patience, verbose=True)
+  valid_losses = []
+  avg_valid_losses = [] 
+  losses_all = []
+  accur_all = []
 
-        #calculate loss
-        
-        loss = loss_fn(output,y_tr.reshape(-1,1))
-        # loss = loss_fn(output,y_train.reshape(-1,1))
-        
-        losses.append(loss.item())
-        #accuracy
-        # predicted = model(torch.tensor(X_train,dtype=torch.float32))
-        # acc = (output.reshape(-1).detach().numpy().round() == y_train.numpy()).mean()
-        acc = (output.reshape(-1).detach().numpy().round() == y_tr.numpy()).mean()
-        accur.append(acc)
-        #backprop
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        # validation
-    losses_all.append(sum(losses)/len(losses))
-    accur_all.append(sum(accur)/len(accur))
-    
+  for i in range(n_epoch):
+      model.train()
+      accur = []
+      losses = []
+      for j,(x_tr,y_tr) in enumerate(trainloader):
+          #calculate output
+          output = model(x_tr)
+          # output = model(x_train)
 
+          #calculate loss
+
+          loss = loss_fn(output,y_tr.reshape(-1,1))
+          # loss = loss_fn(output,y_train.reshape(-1,1))
+
+          losses.append(loss.item())
+          #accuracy
+          # predicted = model(torch.tensor(X_train,dtype=torch.float32))
+          # acc = (output.reshape(-1).detach().numpy().round() == y_train.numpy()).mean()
+          acc = (output.reshape(-1).detach().numpy().round() == y_tr.numpy()).mean()
+          accur.append(acc)
+          #backprop
+          optimizer.zero_grad()
+          loss.backward()
+          optimizer.step()
+          # validation
+      losses_all.append(sum(losses)/len(losses))
+      accur_all.append(sum(accur)/len(accur))
+
+      model.eval()
+      with torch.no_grad():
+          output = model(x_test)
+          loss = loss_fn(output,y_test.reshape(-1,1))
+          acc = (output.reshape(-1).detach().numpy().round() == y_test.numpy()).mean()
     if i%50 == 0:
-        model.eval()
-        with torch.no_grad():
-            output = model(x_test)
-            acc = (output.reshape(-1).detach().numpy().round() == y_test.numpy()).mean()
-        # losses.append(loss)
-        # accur.append(acc)
-        print("epoch {} average train accuracy: {} average loss: {} test accuracy : {}".format(i, accur_all[-1], losses_all[-1],acc))
+      print("epoch {} average train accuracy: {} average loss: {} test accuracy : {}".format(i, accur_all[-1], losses_all[-1],acc))
+    early_stopping(loss, model)
+    if early_stopping.early_stop:
+      print("Early stopping")
+      break
 
 
 
-#plotting the loss
-plt.plot(losses_all)
-plt.title('Loss vs Epochs')
-plt.xlabel('Epochs')
-plt.ylabel('loss')
-plt.savefig("./loss.png")
-plt.show()
+  #plotting the loss
+  plt.plot(losses_all)
+  plt.title('Loss vs Epochs')
+  plt.xlabel('Epochs')
+  plt.ylabel('loss')
+  plt.savefig("./loss.png")
+  plt.show()
 
-plt.plot(accur_all)
-plt.title('Accuracy vs Epochs')
-plt.xlabel('Epochs')
-plt.ylabel('accuracy')
-plt.savefig("./accuracy.png")
-plt.show()
-
-# #Neural Network
-# classifier_NN = MLPClassifier((16, 24, 4), max_iter = 100, solver = 'sgd', alpha = 0.1, activation = 'relu', learning_rate='constant',learning_rate_init=0.1, random_state=42)
-# classifier_NN.fit(X_train, y_train)
-# y_pred_NN = classifier_NN.predict(X_test)
-# print("Accuracy on Neural Network: {0}".format(accuracy_score(y_test, y_pred_NN)))
-# print("Binary Cross-Entropy on Neural Network: {0}".format(log_loss(y_test, y_pred_NN)))
-
-# #Random Forest
-# classifier_RF = RandomForestClassifier(n_estimators = 200, max_depth = 13, n_jobs= -1,     random_state=42)
-# classifier_RF.fit(X_train, y_train)
-# y_pred_RF = classifier_RF.predict(X_test)
-# print("Accuracy on Random Forest Classifier: {0}".format(accuracy_score(y_test, y_pred_RF)))
-# print("Binary Cross-Entropy on Random Forest Classifier: {0}".format(log_loss(y_test, y_pred_RF)))
-
-# #SVM
-# classifier_SV = LinearSVC(C = 0.1, max_iter = 20000)
-# classifier_SV.fit(X_train, y_train)
-# y_pred_SV = classifier_SV.predict(X_test)
-# print("Accuracy on SVM Classifier: {0}".format(accuracy_score(y_test, y_pred_SV)))
-# print("Binary Cross-Entropy on SVM Classifier: {0}".format(log_loss(y_test, y_pred_SV)))
-
+  plt.plot(accur_all)
+  plt.title('Accuracy vs Epochs')
+  plt.xlabel('Epochs')
+  plt.ylabel('accuracy')
+  plt.savefig("./accuracy.png")
+  plt.show()
